cbd502aef669100e91a194b44e5190f3f673ea15
Alberto Ruiz <aruiz@redhat.com>
2024-07-21 18:07:12 +0100

    Attempt to test output

--- test.sh
+++ test.sh
@@ -1,21 +1,26 @@
-#!/bin/bash
+#!/usr/bin/fakeroot bash
 
 TEST_CONTAINER=quay.io/fedora/fedora-bootc
-TEMP=`mktemp -d`
+DESTDIR=`mktemp -d`
 
-./oci2cpio.sh quay.io/fedora/fedora-bootc $TEMP
+./oci2cpio.sh quay.io/fedora/fedora-bootc $DESTDIR
 
-if [ ! -f $TEMP/initramfs.cpio ] ; then
-    rm -rf $TEMP
+if [ ! -f $DESTDIR/initramfs.cpio ] ; then
+    rm -rf $DESTDIR
     echo "bundled cpio file did not exist"
     exit 1
 fi
 
-mkdir $TEMP/tars
-mkdir $TEMP/cpios
+mkdir $DESTDIR/tars
+mkdir $DESTDIR/cpios
 
 for i in `jq '.layers[] | select(.mediaType == "application/vnd.oci.image.layer.v1.tar+gzip") | .digest' $DESTDIR/manifest.json | sed -s s/\"sha256\:// | sed -s s/\"$//`; do
-   tar 
+   tar xvf $DESTDIR/$i -C $DESTDIR/tars > /dev/null
+   cat $DESTDIR/[0-9][0-9].$i.cpio | cpio -idcmv -D $DESTDIR/cpios 
 done
 
-rm -rf $TEMP
+ls -lah $DESTDIR/tars
+ls -lah $DESTDIR/cpios
+
+#TODO: compare trees
+#rm -rf $DESTDIR    ª
